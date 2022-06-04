7392e8727bc9557f53a28cf3e359bb12df96a200
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2022-06-04 16:33:31 +0300

    extension düzenlendi.

--- LoginSample.Data/Extensions/ServiceCollectionExtensions.cs
+++ LoginSample.Data/Extensions/ServiceCollectionExtensions.cs
@@ -1,19 +1,21 @@
 ﻿using Microsoft.Extensions.Configuration;
 using LoginSample.Common;
 using Microsoft.Extensions.DependencyInjection;
+using LoginSample.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoginSample.Data.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    //public static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
-    //{
-    //    var section = configuration.GetSection($"{nameof(Settings)}:Database");
-    //    var settings = section.Get<Settings.DatabaseConfiguration>();
-    //    services.AddDbContext<SpreadDbContext>(builder =>
-    //    {
-    //        builder.UseSqlServer(settings.ConnectionString);
-    //    });
-    //    return services;
-    //}
+    public static IServiceCollection AddData(this IServiceCollection services, IConfiguration configuration)
+    {
+        var section = configuration.GetSection($"{nameof(Settings)}:Database");
+        var settings = section.Get<Settings.DatabaseConfiguration>();
+        services.AddDbContext<LoginSampleDbContext>(builder =>
+        {
+            builder.UseSqlServer(settings.ConnectionString);
+        });
+        return services;
+    }
 }
